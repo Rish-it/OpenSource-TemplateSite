c81620629333401f8a19d5d146eeedfd248d38ca
Rish-it <201b214@juetguna.in>
2024-11-11 15:40:01 +0530

    navbar component added

---
+++ components/Footer/data.ts
@@ -0,0 +1,69 @@
+export const data = [
+  {
+    title: "Company",
+    links: [
+      {
+        name: "About",
+        href: "/"
+      },
+      {
+        name: "Careers",
+        href: "/"
+      },
+      {
+        name: "Press",
+        href: "/"
+      },
+      {
+        name: "Blog",
+        href: "/"
+      },
+      {
+        name: "Community",
+        href: "/"
+      }
+    ]
+  },
+  {
+    title: "Product",
+    links: [
+      {
+        name: "Features",
+        href: "/"
+      },
+      {
+        name: "Integrations",
+        href: "/"
+      },
+      {
+        name: "Pricing",
+        href: "/"
+      },
+      {
+        name: "Security",
+        href: "/"
+      },
+      {
+        name: "Enterprise",
+        href: "/"
+      }
+    ]
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        name: "Twitter",
+        href: "https://twitter.com/irishit_sharma"
+      },
+      {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/rishittsharma/"
+      },
+      {
+        name: "GitHub",
+        href: "https://github.com/rish-it"
+      }
+    ]
+  }
+];
---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  links: {
+    repo: "https://github.com/Rish-it/OpenSource-TemplateSite",
+    x: "https://twitter.com/irishit_sharma",
+    github: "https://github.com/rish-it",
+    linkedin: "https://www.linkedin.com/in/rishittsharma/"
+  },
+  tweets: ["1728096885783552256", "1723490289271046320", "1732180484887626112"]
+};
